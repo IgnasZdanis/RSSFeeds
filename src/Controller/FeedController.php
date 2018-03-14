7ce63a7e36d5cc540171d8d4d7dab26d38ec0a73
IgnasZdanis <zdanis.ignas@gmail.com>
2018-03-14 17:30:55 +0200

    Seeing feeds assigned to a category

---
+++ src/Controller/FeedController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\FeedRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FeedController extends Controller
+{
+    /**
+     * @Route("/feed/{category}", name="feed")
+     */
+    public function index(string $category, FeedRepository $feedRepository)
+    {
+        $feeds = $feedRepository->findByCategory($category);
+        //var_dump($a);
+        return $this->render('feed/index.html.twig', [
+            'feeds' => $feeds,
+        ]);
+    }
+}
