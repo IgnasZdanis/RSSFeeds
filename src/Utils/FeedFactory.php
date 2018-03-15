36305882737cd35631eec31a4058a24d4f97f932
IgnasZdanis <zdanis.ignas@gmail.com>
2018-03-15 17:22:28 +0200

    Moved xml parsing to different class

--- src/Utils/FeedFactory.php
+++ src/Utils/FeedFactory.php
@@ -5,44 +5,26 @@ namespace App\Utils;
 
 
 use App\Entity\Feed;
-use Symfony\Component\DomCrawler\Crawler;
 
 class FeedFactory
 {
+
+    private $rssParser;
+
+    public function __construct(RssParser $rssParser)
+    {
+        $this->rssParser = $rssParser;
+    }
+
     public function createFeedFromUrl(string $url, string $category) : Feed{
         $xml = file_get_contents($url);
-        $crawler = new Crawler($xml);
-        $title = $crawler->filterXPath('//rss/channel/title')->text();
-        $articles = $crawler->filterXPath('//rss/channel/item');
-        $articleCount = $crawler->filterXPath('//rss/channel/item')->count();
-        $latestArticleTitle = null;
-        $latestArticleUrl = null;
-        if ($articleCount) {
-            $maxDate = null;
-            foreach ($articles as $article) {
-                $crawler = new Crawler($article);
-                if ($crawler->filterXPath('//pubDate')->count()) {
-                    $articleDate = new \DateTime($crawler->filterXPath('//pubDate')->text());
-                    if ($maxDate === null || $articleDate > $maxDate) {
-                        $maxDate = $articleDate;
-                        $latestArticleTitle = $crawler->filter('title')->text();
-                        $latestArticleUrl = $crawler->filter('link')->text();
-                    }
-                }
-                else {
-                    $crawler = new Crawler($xml);
-                    $latestArticleTitle = $crawler->filterXPath('//rss/channel/item[1]/title')->text();
-                    $latestArticleUrl = $crawler->filterXPath('//rss/channel/item[1]/link')->text();
-                    break;
-                }
-            }
-        }
+        $info = $this->rssParser->getRssInfo($xml);
         return new Feed(
-            $title,
+            $info['title'],
             $url,
-            $latestArticleTitle,
-            $latestArticleUrl,
-            $articleCount,
+            $info['latestArticleTitle'],
+            $info['latestArticleUrl'],
+            $info['articleCount'],
             new \DateTime(),
             $category
         );
---
+++ src/Utils/RssParser.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\Utils;
+
+
+use Symfony\Component\DomCrawler\Crawler;
+
+class RssParser
+{
+    public function getRssInfo(string $xml) : array
+    {
+        $info = [];
+        $crawler = new Crawler($xml);
+        $articleCount = $crawler->filterXPath('//rss/channel/item')->count();
+        $latestArticleInfo = $this->getLatestArticleInfo($articleCount, $xml);
+        $info['title'] = $crawler->filterXPath('//rss/channel/title')->text();
+        $info['articleCount'] = $crawler->filterXPath('//rss/channel/item')->count();
+        $info['latestArticleTitle'] = $latestArticleInfo['title'];
+        $info['latestArticleUrl'] = $latestArticleInfo['url'];
+        return $info;
+
+    }
+
+    private function getLatestArticleInfo(int $articleCount, string $xml) : array{
+        $crawler = new Crawler($xml);
+        $info['title'] = null;
+        $info['url'] = null;
+        $articles = $crawler->filterXPath('//rss/channel/item');
+        if ($articleCount) {
+            $maxDate = null;
+            foreach ($articles as $article) {
+                $crawler = new Crawler($article);
+                if ($crawler->filterXPath('//pubDate')->count()) {
+                    $articleDate = new \DateTime($crawler->filterXPath('//pubDate')->text());
+                    if ($maxDate === null || $articleDate > $maxDate) {
+                        $maxDate = $articleDate;
+                        $info['title'] = $crawler->filter('title')->text();
+                        $info['url'] = $crawler->filter('link')->text();
+                    }
+                }
+                else {
+                    $crawler = new Crawler($xml);
+                    $info['title'] = $crawler->filterXPath('//rss/channel/item[1]/title')->text();
+                    $info['url'] = $crawler->filterXPath('//rss/channel/item[1]/link')->text();
+                    break;
+                }
+            }
+        }
+        return $info;
+    }
+
+}
